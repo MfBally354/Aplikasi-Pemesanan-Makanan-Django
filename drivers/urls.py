d0e70d4c8316e3068d170e2dec362b03be163b23
zakyzexo <zakyzexo@gmail.com>
2025-12-11 18:49:00 +0700

    error part 2

--- drivers/urls.py
+++ drivers/urls.py
@@ -6,7 +6,7 @@ app_name = 'drivers'
 urlpatterns = [
     path("dashboard/", views.driver_dashboard, name="driver_dashboard"),
     path("orders/available/", views.driver_available_orders, name="driver_available_orders"),
-    path("orders/<int:order_id>/accept/", views.driver_accept_order, name="driver_accept_order"),
-    path("orders/<int:order_id>/update/", views.driver_update_status, name="driver_update_status"),
+    path("take-order/<int:order_id>/", views.take_order, name="take_order"),
+    path("update-status/<int:order_id>/", views.update_status, name="update_status"),
     path("history/", views.driver_history, name="driver_history"),
 ]
